02b3c5a40eac08bc6b5e3d47542562ef3a0db540
ricardo <ricardo.etcheto@gmail.com>
2026-05-26 16:51:40 -0300

    agrego swagger

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express		from "express"
 import router		from "./routes/router.js"
 import errorHandler from "./errors/ErrorHandler.js"
 import notFoundHandler from "./errors/NotFoundHandler.js"
+import swaggerUi from "swagger-ui-express"
+import swaggerSpec from "./swagger.js"
 
 const app = express()
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(router);
 app.use(notFoundHandler);
 app.use(errorHandler);
