acc80cf0677130064281da19bbf086b68704aee2
ricardo <ricardo.etcheto@gmail.com>
2026-06-21 00:57:17 -0300

    agrego config de axios GLOBAL para no tener que llamar al api.js. Despues hay que sacar ese axios.create, por ahora lo dejo para que no rompa

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3001';
+
+// Configuración global de axios
+axios.defaults.baseURL = BASE_URL;
+// axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.timeout = process.env.BASE_URL || 10000;
 
 const api = axios.create({
-	baseURL: 'http://localhost:3001',
+	baseURL: BASE_URL,
 	headers: {
 		"Content-Type": "application/json"
 	},
