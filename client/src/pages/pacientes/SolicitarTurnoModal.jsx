edf15eb13d17733c0c1c21254860928dcf42cb4a
Francisco Agustín García Araya <fgarciaaraya@frba.utn.edu.ar>
2026-06-23 14:12:42 -0300

    hotfix error

--- client/src/pages/pacientes/SolicitarTurnoModal.jsx
+++ client/src/pages/pacientes/SolicitarTurnoModal.jsx
@@ -166,12 +166,6 @@ export default function SolicitarTurnoModal({ show, seleccionado, onHide }) {
                         </div>
                     </Card.Body>
                 </Card>
-                {error && (
-                    <Alert variant="danger">{error}</Alert>
-                )}
-                {turnoSolicitado && (
-                    <Alert variant="success">Turno solicitado!!</Alert>
-                )}
             </Modal.Body>
 
             <Modal.Footer className="flex-wrap gap-2">
