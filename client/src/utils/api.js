7cbf0f495996149e066408d5e0c41f93de2af344
ricardo <ricardo.etcheto@gmail.com>
2026-07-07 10:34:41 -0300

    agrego el /api para la base url en local y commiteo a proposito el .env.production que tiene solo la base url, no tiene datos sensibles

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3001';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 
 // Configuración global de axios
 axios.defaults.baseURL = BASE_URL;
 // axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.timeout = process.env.BASE_URL || 10000;
+axios.defaults.timeout = 10000;
 
 const api = axios.create({
 	baseURL: BASE_URL,
